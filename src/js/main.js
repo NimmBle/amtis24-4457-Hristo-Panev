b43c7c6a45debb0fbc9b63d1b52bd05cb9175a39
bunnyt29 <taniabojkova929@gmail.com>
2024-06-01 15:15:31 +0300

    save

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
 // Продължи с твоя vanilla JavaScript код тук.
+
+
+// async function getAllData() {
+
+//   const 
+// }
+
+// const mobileToggle = document.querySelector('.mobile-nav-toggle');
+// const iconHamburger = document.querySelector('.icon-hamburger');
+// const iconClose = document.querySelector('.icon-close');
+// mobileToggle.addEventListener('click', () => {
+
+// })
+
+const hamburger = document.querySelector('.icon-hamburger');
+const navLinks = document.querySelector(".primary-navigation");
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle("hidden");
+    hamburger.classList.toggle("open");
+});
