82bee2678161ed009278d00c8365f47530f78793
bunnyt29 <taniabojkova929@gmail.com>
2024-06-01 16:40:06 +0300

    final commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,33 @@
 // Продължи с твоя vanilla JavaScript код тук.
 
 
-// async function getAllData() {
+async function getAllData() {
+  const response = await fetch("http://localhost:4000/desserts");
+  const desserts = await response.json();
 
-//   const 
-// }
+  var allDesesrtNames = document.querySelectorAll('.dessert-name'); 
+  var allShortDescriptions = document.querySelectorAll('.description--showcase');
+  var allIngridiens = document.querySelectorAll('.ingridientsToChange');
+  var allImagesShowcase = document.querySelectorAll('.sourceOfImageToChangeShowcase');
+  var allImagesDetails = document.querySelectorAll('.sourceOfImageToChange');
 
-// const mobileToggle = document.querySelector('.mobile-nav-toggle');
-// const iconHamburger = document.querySelector('.icon-hamburger');
-// const iconClose = document.querySelector('.icon-close');
-// mobileToggle.addEventListener('click', () => {
 
-// })
+  // console.log(allImages[0].srcset);
+  for (let index = 0; index < allDesesrtNames.length; index++) {
+    allDesesrtNames[index].innerHTML = desserts[index].name;
+    allShortDescriptions[index].innerHTML = desserts[index].description_short;
+    allIngridiens[index].innerHTML = desserts[index].ingredients_text;
+    allImagesShowcase[index].srcset = allImagesShowcase[index].srcset + desserts[index].image;
+    allImagesDetails[index].srcset = allImagesDetails[index].srcset + desserts[index].image;
+  }
+
+}
+getAllData();
+
 
 const hamburger = document.querySelector('.icon-hamburger');
 const navLinks = document.querySelector(".primary-navigation");
 hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle("hidden");
+    navLinks.classList.toggle("Active");
     hamburger.classList.toggle("open");
 });
